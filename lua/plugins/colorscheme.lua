74ce474103cd860e180b215cfb21a98170e08eeb
Ironmar <fmrm93@gmail.com>
2023-12-12 23:47:52 -0500

    Add configs nvim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = { flavour = "macchiato" } },
+  { "sainnhe/gruvbox-material", name = "gruvbox-material", lazy = false },
+  -- { "olimorris/onedarkpro.nvim", name = "onedarkpro", lazy = false },
+  { "rmehri01/onenord.nvim", name = "onenord", lazy = false },
+  { "navarasu/onedark.nvim", name = "onedark", lazy = false, opts = { style = "warm" } },
+  { "folke/tokyonight.nvim", name = "tokyonight", lazy = false },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
